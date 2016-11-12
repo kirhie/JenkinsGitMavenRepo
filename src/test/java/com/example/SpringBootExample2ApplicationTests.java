f30f257949e299d10080d92e31d96f991ac7f771
kirhie <kkiranshinde@gmail.com>
2016-11-12 22:42:46 +0530

    Commit from Source Tree

--- src/test/java/com/example/SpringBootExample2ApplicationTests.java
+++ src/test/java/com/example/SpringBootExample2ApplicationTests.java
@@ -8,9 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootExample2Application.class)
 public class SpringBootExample2ApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }
